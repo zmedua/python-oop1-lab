9d2ed1dd6cee5d311af1488934aa3987c5f788a7
Zane <zimedua@gmail.com>
2026-06-01 21:35:35 -0500

    lab oop1

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else: print("page_count must be an integer")
+
+    def turn_page(self):
+       print("Flipping the page...wow, you read fast!")
+            
     
         
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size =size
+        self.price = price
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if value in ["Small", "Medium", "Large"]:
+            self._size = value
+        else: print("size must be Small, Medium, or Large")
+
+    def tip(self):
+        self.price += 1
+        print("This coffee is great, here’s a tip!")
+
